58ef5c0887627bf2d37190c59326d794e074e6ca
KLCT <s7unn0lo>
2020-06-21 21:55:41 -0500

    completed 1st assignment

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.console;
 
+import java.text.CollationElementIterator;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Map;
+import java.util.Collections;
+
 
 /**
  * Created by LaunchCode
@@ -10,6 +14,8 @@ import java.util.Scanner;
 public class TechJobs {
 
     private static Scanner in = new Scanner(System.in);
+    //copy of allJobs
+    private static ArrayList<HashMap<String,String>> copyAllJobs = JobData.findAll();
 
     public static void main (String[] args) {
 
@@ -38,10 +44,11 @@ public class TechJobs {
                 String columnChoice = getUserSelection("List", columnChoices);
 
                 if (columnChoice.equals("all")) {
-                    printJobs(JobData.findAll());
+                    printJobs(copyAllJobs);
                 } else {
 
                     ArrayList<String> results = JobData.findAll(columnChoice);
+                    Collections.sort(results);
 
                     System.out.println("\n*** All " + columnChoices.get(columnChoice) + " Values ***");
 
@@ -61,7 +68,8 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    //where to implement findByValue method
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -111,6 +119,21 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        //added if/else for validity
+        if(!someJobs.isEmpty()) {
+            //created method
+            for (HashMap<String,String> jobs : someJobs) {
+                System.out.println("*****");
+
+                for(Map.Entry<String,String> job : jobs.entrySet()) {
+                    System.out.println(job.getKey() + ": " + job.getValue());
+                }
+                System.out.println("*****\n");
+            }
+        } else {
+            System.out.println("searchterm not found");
+        }
+
     }
 }
+
